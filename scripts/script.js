2fc804f5066ad54c55742a2086df817938fd2e74
GuminskiAdrian <guminskiadrian34@gmail.com>
2023-01-26 15:29:17 +0100

    added id to new nodes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
-const workoutContainer = document.getElementById('workoutContainer');
-const addButton = document.getElementById('addButton');
-const removeButton = document.getElementById('removeButton');
-const submit = document.getElementById('submit');
-const template = document.getElementById('template');
+const addButton = document.getElementById("addButton");
+const removeButton = document.getElementById("removeButton");
+const submit = document.getElementById("submit");
 
-addButton.addEventListener('click', () => {
+const workoutContainer = document.getElementById("workoutContainer");
+const template = document.getElementById("template");
+
+let number = 5;
+
+addButton.addEventListener("click", (e) => {
     const addTemplate = document.importNode(template.content, true);
     workoutContainer.appendChild(addTemplate);
-})
+    workoutContainer.lastElementChild.setAttribute('id', `nr${number.toString()}`)
+    number++;
+});
+
+removeButton.addEventListener("click", (e) => {
+    workoutContainer.removeChild(workoutContainer.lastChild);
+});
