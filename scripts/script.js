2bbf56c7b795a6d1f118f4b91759198fe8eefb56
GuminskiAdrian <guminskiadrian34@gmail.com>
2023-01-30 13:07:22 +0100

    changed flex-box inside workout class to grid and updated js buttons so it works after changed DOM

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,15 @@ const submit = document.getElementById("submit");
 const workoutContainer = document.getElementById("workoutContainer");
 const template = document.getElementById("template");
 
-let number = 5;
-
 addButton.addEventListener("click", (e) => {
     const addTemplate = document.importNode(template.content, true);
     workoutContainer.appendChild(addTemplate);
-    workoutContainer.lastElementChild.setAttribute('id', `nr${number.toString()}`)
-    number++;
 });
 
 removeButton.addEventListener("click", (e) => {
-    workoutContainer.removeChild(workoutContainer.lastChild);
+    let childsNr = workoutContainer.childElementCount - 1;
+    const limit = childsNr - 3;
+    for (childsNr; childsNr >= limit; childsNr--) {
+        workoutContainer.removeChild(workoutContainer.children[childsNr]);
+    }
 });
